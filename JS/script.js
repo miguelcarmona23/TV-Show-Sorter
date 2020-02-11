21818a329515b394352741a38e1079fac3cb0b36
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 14:25:03 -0500

    Added querySelector for #tvShows

--- JS/script.js
+++ JS/script.js
@@ -20,4 +20,9 @@ function strip(tvShowNames) {
 
 const sortedTvShows = tvShows.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
+document.querySelector('#tvshows').innerHTML =
+    sortedTvShows
+    .map(tvShows => `<li>${tvShows}</li>`)
+    .join('');
+
 console.log(sortedTvShows);
