e8dbe7017d083849a3b0042707ff430a415ede80
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 14:10:04 -0500

    Formatted const tvShows array

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-const tvShows = ['The Simpsons', 'Family Guy', 'Bobs Burgers', 'Rick and Morty', 'South Park', 'American Dad', 'Futurama', 'Spongebob Squarepants', 'Boy Meets World', 'Friends', 'How I Met Your Mother', 'The Fresh Prince of Bel Air', 'The Office'];
+const tvShows = [
+    "The Simpsons",
+    "Family Guy",
+    "Bobs Burgers",
+    "Rick and Morty",
+    "South Park",
+    "American Dad",
+    "Futurama",
+    "Spongebob Squarepants",
+    "Boy Meets World",
+    "Friends",
+    "How I Met Your Mother",
+    "The Fresh Prince of Bel Air",
+    "The Office"
+];
