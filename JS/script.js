cc3470497b8f0f3d7fb7b571a7900883a41d89cc
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 13:54:52 -0500

    Added const tvshows

---
+++ JS/script.js
@@ -0,0 +1,1 @@
+const tvshows = ['The Simpsons', 'Family Guy', 'Bobs Burgers', 'Rick and Morty', 'South Park', 'American Dad', 'Futurama', 'Spongebob Squarepants', 'Boy Meets World', 'Friends', 'How I Met Your Mother', 'The Fresh Prince of Bel Air', 'The Office'];
